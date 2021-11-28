b31cbc94e5bd8430a2c9a06e2e26c1acbedc48aa
Riderpool1 <akshayashokan1054.com>
2021-11-28 12:47:27 +0530

    feature/19/in-app-update

--- app/src/main/java/com/akshayAshokCode/textrecognition/MainActivity.java
+++ app/src/main/java/com/akshayAshokCode/textrecognition/MainActivity.java
@@ -3,6 +3,7 @@ package com.akshayAshokCode.textrecognition;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
@@ -12,10 +13,19 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentManager;
+
+import com.google.android.play.core.appupdate.AppUpdateInfo;
+import com.google.android.play.core.appupdate.AppUpdateManager;
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory;
+import com.google.android.play.core.install.model.AppUpdateType;
+import com.google.android.play.core.install.model.UpdateAvailability;
+import com.google.android.play.core.tasks.Task;
 
 // Add image crop feature, Add copy button, add font identifying button
 public class MainActivity extends AppCompatActivity {
@@ -23,6 +33,14 @@ public class MainActivity extends AppCompatActivity {
     FrameLayout layout;
     FragmentManager fragmentManager;
     private static final String TAG = "MainActivity";
+    private final int REQUEST_CODE=11;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        checkUpdate();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,4 +83,36 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    private void checkUpdate(){
+        AppUpdateManager appUpdateManager = AppUpdateManagerFactory.create(this);
+
+// Returns an intent object that you use to check for an update.
+        Task<AppUpdateInfo> appUpdateInfoTask = appUpdateManager.getAppUpdateInfo();
+
+// Checks that the platform will allow the specified type of update.
+        appUpdateInfoTask.addOnSuccessListener(appUpdateInfo -> {
+            if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+                    // This example applies an immediate update. To apply a flexible update
+                    // instead, pass in AppUpdateType.FLEXIBLE
+                    && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)) {
+                // Request the update.
+                try {
+                    appUpdateManager.startUpdateFlowForResult(appUpdateInfo,AppUpdateType.IMMEDIATE,this,REQUEST_CODE);
+                } catch (IntentSender.SendIntentException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==REQUEST_CODE){
+            Log.d(TAG, "onActivityResult: Updated to Latest Features");
+            if(resultCode!=RESULT_OK){
+                Log.e(TAG,"Update Failed, please Go to Google Play Store & update.");
+            }
+        }
+    }
+
 }
